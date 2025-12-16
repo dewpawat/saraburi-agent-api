65064ae5f4da28041d20c4b83acd24e8a10ac75b
Pawat Tanittikul <dewjan007@hotmail.com>
2025-12-16 15:41:02 +0700

    16/15/2568 - Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,16 @@
 from fastapi import Request, Header, HTTPException
 from app.core.config import settings
 
+def get_client_ip(request: Request) -> str:
+    # 1) ใช้ X-Forwarded-For ก่อน (จาก Apache / Proxy)
+    xff = request.headers.get("x-forwarded-for")
+    if xff:
+        # กรณีมีหลาย proxy → เอาตัวแรกสุด (client จริง)
+        return xff.split(",")[0].strip()
+
+    # 2) fallback กรณีไม่ผ่าน proxy
+    return request.client.host
+
 async def api_security(request: Request, body_hospcode: str):
     x_api_key = request.headers.get("x-api-key")
     hospcode_header = request.headers.get("x-hospcode")
@@ -20,7 +30,8 @@ async def api_security(request: Request, body_hospcode: str):
         raise HTTPException(status_code=400, detail="Hospcode not allowed")
 
     # 3. ตรวจ IP Address
-    client_ip = request.client.host
+    # client_ip = request.client.host
+    client_ip = get_client_ip(request)
     allowed_ips = {settings.API_ALLOWED_IP1, settings.API_ALLOWED_IP2}
 
     # ลบ None ออกจาก set
